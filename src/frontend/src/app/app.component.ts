79f94e1632ce541dd2b43afc11b0cba5311286e5
Cristian_Recabarren <109612110+bit-rec98@users.noreply.github.com>
2025-02-17 15:49:39 -0300

    Added initial frontend structure development

--- src/frontend/src/app/app.component.ts
+++ src/frontend/src/app/app.component.ts
@@ -1,13 +1,16 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from './layout/components/header/header.component';
+import { SidebarComponent } from './layout/components/sidebar/sidebar.component';
+import { FooterComponent } from './layout/components/footer/footer.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HeaderComponent, SidebarComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'frontend';
+  title = 'AI Local Assistant';
 }
--- src/frontend/src/app/app.routes.ts
+++ src/frontend/src/app/app.routes.ts
@@ -1,3 +1,39 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'login',
+    loadComponent: () =>
+      import('./pages/login/login.component').then((m) => m.LoginComponent),
+  },
+
+  {
+    path: 'register',
+    loadComponent: () =>
+      import('./pages/register/register.component').then((m) => m.RegisterComponent),
+  },
+
+  {
+    path: '',
+    loadComponent: () =>
+      import('./pages/home/home.component').then((m) => m.HomeComponent),
+  },
+
+  {
+    path: 'about',
+    loadComponent: () =>
+      import('./pages/about/about.component').then((m) => m.AboutComponent),
+  },
+
+  {
+    path: 'contact',
+    loadComponent: () =>
+      import('./pages/contact/contact.component').then((m) => m.ContactComponent),
+  },
+
+  {
+    path: 'chat',
+    loadComponent: () =>
+      import('./pages/chat/chat.component').then((m) => m.ChatComponent),
+  }
+];
