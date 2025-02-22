8cb77d051fd802a7a6efdc180a893597a10456b9
Cristian_Recabarren <109612110+bit-rec98@users.noreply.github.com>
2025-02-22 11:08:52 -0300

    Added development to features

--- src/frontend/src/app/pages/chat/chat.component.ts
+++ src/frontend/src/app/pages/chat/chat.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-chat',
@@ -7,6 +7,14 @@ import { Component } from '@angular/core';
   templateUrl: './chat.component.html',
   styleUrl: './chat.component.scss'
 })
-export class ChatComponent {
+export class ChatComponent implements OnInit {
+
+  constructor(){
+
+  }
+
+  ngOnInit(): void {
+
+  }
 
 }
